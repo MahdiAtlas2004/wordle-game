a41cce76f449d4ddcd2b1db6d0abc8a2daac1af4
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-16 22:47:04 +0430

    db.json file added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json => {
+      //random int between 0 & 14
+      const randomSolution = json[Math.floor(Math.random() * json.length)];
+      setSolution(randomSolution.word);
+    })
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
@@ -48,3 +62,8 @@ game process:
       -- modal to say 'unlucky'
 
 */
+
+// solution data:
+// 1- 3rd party api
+// 2- own database (e.g mongodb)
+// 3- json file
