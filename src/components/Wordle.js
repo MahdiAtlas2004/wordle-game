19c97c09435bba32bc7c2857c1d4c328d96c4529
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-17 11:09:38 +0430

    keep tracking of the current guess that user typing

---
+++ src/components/Wordle.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
+
+const Wordle = ({solution}) => {
+    const {currentGuess, handleKeyup} = useWordle(solution);
+    
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyup)
+
+        // this return statement prevent of execution several handle keyup function
+        return () => window.removeEventListener('keyup', handleKeyup)
+    }, [handleKeyup])
+
+    return (
+        <div>current guess: {currentGuess}</div>
+    )
+}
+ 
+export default Wordle;
