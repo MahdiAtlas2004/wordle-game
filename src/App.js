0dbe8bc94c30b019af1d70198a685770347da891
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-17 13:01:49 +0430

    checking and formatting the current guess

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   const [solution, setSolution] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
-    .then(res => res.json())
-    .then(json => {
-      //random int between 0 & 14
-      const randomSolution = json[Math.floor(Math.random() * json.length)];
-      setSolution(randomSolution.word);
-    })
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        //random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
   }, [setSolution]);
 
   return (
@@ -22,7 +22,7 @@ function App() {
   );
 }
 
-export default App
+export default App;
 
 /* 
 
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,18 +1,21 @@
 import { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 
-const Wordle = ({solution}) => {
-    const {currentGuess, handleKeyup} = useWordle(solution);
-    
-    useEffect(() => {
-        window.addEventListener('keyup', handleKeyup)
-        // this return statement prevent of execution several handle keyup function
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup])
+const Wordle = ({ solution }) => {
+  const { currentGuess, handleKeyup } = useWordle(solution);
 
-    return (
-        <div>current guess: {currentGuess}</div>
-    )
-}
- 
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+    // this return statement prevent of execution several handle keyup function
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
+  return (
+    <div>
+      <div>Solution - {solution}</div>
+      <div>current guess: {currentGuess}</div>
+    </div>
+  );
+};
+
 export default Wordle;
