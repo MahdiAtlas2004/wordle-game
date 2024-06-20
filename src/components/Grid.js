166b03ffcf564ae207b8b24c46f3045d47c7151d
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-20 15:58:53 +0430

    Showing the current guess

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,11 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
       {guesses.map((g, i) => {
+        // checking if there is a current guess that user typing
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
+        // returning different rows
         return <Row key={i} guess={g} />;
       })}
     </div>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
+
+  // returning the past guesses
   if (guess) {
     return (
       <div className="row past">
@@ -11,6 +13,24 @@ export default function Row({ guess }) {
     )
   }
 
+  if(currentGuess) {
+    // converting the current guess string into an array of letters
+    let letters = currentGuess.split('');
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">{letter}</div>
+        ))}
+        {/* Showing the empty boxes */}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    )
+  }
+
+  // the defualt return value when there is no value
   return (
     <div className="row">
       <div></div>
