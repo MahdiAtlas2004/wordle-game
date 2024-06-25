9ca9a16b7265b87a4d63e9d143e828a4e003681c
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-25 06:06:11 +0430

    Information modal has been completed

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -2,8 +2,8 @@ const InfoModal = ({handleInfo}) => {
   return (
     <div className="info-modal">
       <div className="info-modal-content">
-        <i class="bi bi-x-lg exit-icon" onClick={handleInfo}></i>
-        <h1>How To Play</h1>
+        <i className="bi bi-x-lg exit-icon" onClick={handleInfo}></i>
+        <h1 className="how-to">How To Play</h1>
         <h2>Guess the Wordle in 6 tries.</h2>
         <ul>
           <li>Each guess must be a valid 5-letter word</li>
@@ -15,7 +15,7 @@ const InfoModal = ({handleInfo}) => {
         <h3>Examples</h3>
         <div className="example">
           <div className="info-row">
-            <div className="info-col">W</div>
+            <div className="info-col" id="e1">W</div>
             <div className="info-col">E</div>
             <div className="info-col">A</div>
             <div className="info-col">R</div>
@@ -27,7 +27,7 @@ const InfoModal = ({handleInfo}) => {
         <div className="example">
           <div className="info-row">
             <div className="info-col">P</div>
-            <div className="info-col">I</div>
+            <div className="info-col" id="e2">I</div>
             <div className="info-col">L</div>
             <div className="info-col">L</div>
             <div className="info-col">S</div>
@@ -40,7 +40,7 @@ const InfoModal = ({handleInfo}) => {
             <div className="info-col">V</div>
             <div className="info-col">A</div>
             <div className="info-col">G</div>
-            <div className="info-col">U</div>
+            <div className="info-col" id="e3">U</div>
             <div className="info-col">E</div>
           </div>
           <p>U is not in the word in any spot.</p>
