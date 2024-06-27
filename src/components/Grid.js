1ddaa7bde2b35b58171bb83aba060a9228b8dcb3
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-27 06:36:24 +0430

    displaying keyboard function added to gird component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,10 +2,17 @@ import React from "react";
 import Row from "./Row";
 
 export default function Grid({ currentGuess, guesses, turn }) {
+  const handleClick = () => {
+    const editableDiv = document.getElementById('grid');
+    if (editableDiv) {
+      editableDiv.focus();
+    }
+  }
+
   return (
-    <div>
+    <div id="grid" onClick={handleClick} tabIndex="0">
       {guesses.map((g, i) => {
-        // checking if there is a current guess that user typing
+        // checking if there is a current guess that user is typing
         if (turn === i) {
           return <Row key={i} currentGuess={currentGuess} />;
         }
